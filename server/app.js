dc9d2ee8ef2237b76fdcd5cc4d71f633d706b96e
talia ben hamo <106983102+Taliabenhamo@users.noreply.github.com>
2024-01-14 17:45:19 +0200

    initinal

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const userRouter = require('./routers/users');
 const cardRouter = require('./routers/cards');
-const port = 5000;
+const port = 3001;
 const cors = require('cors');
 require('./dal/dal');
 
